87c5819ffd519f7907e230817aae70cd53184e33
janci-osadsky <osadsky2@uniba.sk>
2023-11-20 10:58:26 +0100

    Pattern Line placeholder

---
+++ src/main/java/sk/uniba/fmph/dcs/PatternLine.java
@@ -0,0 +1,30 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class PatternLine {
+    private int capacity;
+    private List<Tile> tiles;
+    public PatternLine(int capacity){
+        this.capacity = capacity;
+        this.tiles = new ArrayList<>();
+    }
+    public void put(final Collection<Tile> tiles) {
+        this.tiles.addAll(tiles);
+    }
+
+    public Points finishRound(){
+        return null;
+    }
+    public String state(){
+        String toReturn = "";
+        for (final Tile tile : tiles) {
+            toReturn += tile.toString();
+        }
+        toReturn += " ";
+        return "";
+    }
+
+}
